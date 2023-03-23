73c51ae8fc6e3f4b67bcc1c60b942b9e2b84857d
Lucas Gabriel Ruiz <lucaschatoruiz@gmail.com>
2023-03-23 15:10:31 -0300

    carrousel agregado al home

--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,4 +1,6 @@
 import React from 'react'
+import { Carousel } from 'react-carousel-minimal';
+
 import Header from '../../Components/Header'
 
 import monte1 from '../../Assets/Images/monte1.jpg'
@@ -10,15 +12,56 @@ import './style.css'
 
 const Home = () => {
 
-  
+  const data = [
+    {
+      image: monte1,
+    },
+    {
+      image: monte2,
+    },
+    {
+      image: monte3,
+    },
+    {
+      image: monte4,
+    },
+    // {
+    //   image: monte1,
+    // },
+    // {
+    //   image: monte1,
+    // },
+    // {
+    //   image: monte1,
+    // }
+  ];
+
   return (
     <>
       <Header />
       <div className="carrousel">
-        <img src={monte1} className="img" alt="NO " />
-        <img src={monte2} className="img" alt="NO " />
-        <img src={monte3} className="img" alt="NO " />
-        <img src={monte4} className="img" alt="NO " />
+        <Carousel
+          data={data}
+          time={3000}
+          width="40rem"
+          height="40rem"
+          radius="2rem"
+          captionPosition="bottom"
+          automatic={true}
+          dots={true}
+          pauseIconColor="white"
+          pauseIconSize="2.5rem"
+          slideBackgroundColor="darkgrey"
+          slideImageFit="cover"
+          thumbnails={true}
+          thumbnailWidth="100rem"
+          thumbnailHeight="100rem"
+          style={{
+            margin: "2rem auto",
+            cursor: "pointer",
+            width: "50rem"
+          }}
+        />
       </div>
     </>
   )
